766e25a03b6f7ed548ad9a4aca34b0d8f67d69c3
fanshukai <fanshukai@fanshukais-MacBook-Pro.local>
2021-02-20 00:45:37 +0800

    middle

---
+++ Problem_Set_4_JS/ps4-my-code.js
@@ -0,0 +1,28 @@
+const btn1 = document.getElementById("Btn1");
+const btn2 = document.getElementById("Btn2");
+const text1 = document.getElementById("Text1");
+const reply = document.getElementById("Reply");
+const input = document.querySelector('input');
+
+btn1.addEventListener("click", event1);
+input.addEventListener('input', event2);
+btn2.addEventListener("mouseover", event3);
+
+var r = document.querySelector(':root');
+
+function event1() {
+    text1.innerHTML = "I mean forever!";
+}
+
+function event2(e) {
+    if(e.target.value == "JYT"){
+        reply.textContent = "Alert! Wife Comming! Girls Out!";
+    }
+    else{
+        reply.textContent = "nice to meet you " + e.target.value;
+    }
+}
+
+function event3() {
+    r.style.setProperty('--main-bg-color', 'green');
+}
